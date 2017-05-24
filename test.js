2c6c1c59fc1d1b73edc6cfe937319d8639a8d2da
Kevin Martensson <kevinmartensson@gmail.com>
2017-05-25 01:47:07 +0200

    Remove `npm` dependency (#2)

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
+import npmCore from 'npm/lib/config/core';
+import npmDefaults from 'npm/lib/config/defaults';
+import pify from 'pify';
 import test from 'ava';
 import m from '.';
 
-test(async t => {
-	const conf = await m();
-	t.truthy(conf.get('prefix'));
+test('mirror npm config', async t => {
+	const conf = m();
+	const npmconf = await pify(npmCore.load)();
+
+	t.is(conf.get('globalPrefix'), npmconf.get('globalPrefix'));
+	t.is(conf.get('localPrefix'), npmconf.get('localPrefix'));
+	t.is(conf.get('prefix'), npmconf.get('prefix'));
+	t.is(conf.get('proxy'), npmconf.get('proxy'));
+	t.is(conf.get('registry'), npmconf.get('registry'));
+	t.is(conf.get('tmp'), npmconf.get('tmp'));
+});
+
+test('mirror npm defaults', t => {
+	t.deepEqual(m.defaults, npmDefaults.defaults);
 });
